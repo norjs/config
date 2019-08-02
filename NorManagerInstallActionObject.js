2c3253b06af07a73c9f84dda1d5cbeda794c9c5e
Jaakko Heusala <jheusala@iki.fi>
2019-08-02 07:50:44 +0300

    Added new option for manager install object

--- NorManagerInstallActionObject.js
+++ NorManagerInstallActionObject.js
@@ -7,11 +7,13 @@ const TypeUtils = require("@norjs/utils/Type");
  * @property  {string} [name] - The service name to install
  * @property {boolean} [production] - If enabled, only production services will be used
  * @property {boolean} [development] - If enabled, only development services will be used
+ * @property {boolean} [stdout] - If enabled, will try to capture stdout from the install command. Defaults to false.
  *
  */
 TypeUtils.defineType("NorManagerInstallActionObject", {
     "name"        : "string",
     "production"  : "boolean",
-    "development" : "boolean"
+    "development" : "boolean",
+    "stdout"      : "boolean"
 });
 
