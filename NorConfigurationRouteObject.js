de48be2cf1eadd66eb8207d37aeb238c058bafae
Jaakko Heusala <jheusala@iki.fi>
2019-08-04 14:19:35 +0300

    Updated route configuration

--- NorConfigurationRouteObject.js
+++ NorConfigurationRouteObject.js
@@ -3,12 +3,14 @@ const TypeUtils = require("@norjs/utils/Type");
 
 /**
  * @typedef {Object} NorConfigurationRouteObject
- * @property {string} [service] - The service name
- * @property {string} [socket] - The socket file name
+ * @property {string} [service] - The service name (This is not used at the moment.)
+ * @property {string} [socket] - The socket file name to route the request to
+ * @property {string} [target] - The target address to route the request to; is socket is undefined
  * @property {string} [auth] - Which authentication configuration to use
  */
 TypeUtils.defineType("NorConfigurationRouteObject", {
     "service": "string",
     "socket": "string",
+    "target": "string",
     "auth": "string"
 });
