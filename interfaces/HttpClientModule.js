bb5c9bb5832390d153b847109aa622475047b53d
Jaakko Heusala <jheusala@iki.fi>
2019-09-01 15:28:38 +0300

    Updated types

--- interfaces/HttpClientModule.js
+++ interfaces/HttpClientModule.js
@@ -10,16 +10,43 @@ const TypeUtils = require("@norjs/utils/Type");
 
 /**
  * @typedef {Object} HttpClientOptionsObject
- * @prototype {string} method -
- * @prototype {string} path -
- * @prototype {string} socketPath -
+ *
+ * @prototype {Object|boolean} agent -
+ * @prototype         {string} auth - Eg. `user:password`
+ * @prototype       {Function} createConnection
+ * @prototype         {number} defaultPort -
+ * @prototype         {number} family - Eg. `4` or `6`
+ * @prototype         {Object} headers -
+ * @prototype         {string} host -
+ * @prototype         {string} hostname - Alias for `host`, try not to use if possible.
+ * @prototype         {string} localAddress -
+ * @prototype         {string} method -
+ * @prototype         {string} path - Eg. `/index.html?page=3`
+ * @prototype         {number} port -
+ * @prototype         {string} protocol - Eg. `http:`
+ * @prototype        {boolean} setHost -
+ * @prototype         {string} socketPath -
+ * @prototype         {number} timeout - Milliseconds
  */
 TypeUtils.defineType(
     "HttpClientOptionsObject",
     {
-        "method": "string",
-        "path": "string",
-        "socketPath": "string"
+        "agent": "object|boolean|undefined",
+        "auth": "string|undefined",
+        "createConnection": "Function|undefined",
+        "defaultPort": "number|undefined",
+        "family": "number|undefined",
+        "headers": "object|undefined",
+        "host": "string|undefined",
+        "hostname": "string|undefined",
+        "localAddress": "string|undefined",
+        "method": "string|undefined",
+        "path": "string|undefined",
+        "port": "number|undefined",
+        "protocol": "string|undefined",
+        "setHost": "boolean|undefined",
+        "socketPath": "string|undefined",
+        "timeout": "number|undefined"
     },
     {
         acceptUndefinedProperties: true
